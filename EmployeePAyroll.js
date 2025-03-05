56639d0703aa7ab8e86fd024ae1c19fabd3adf74
drshny03 <darshanyadav7049@gmail.com>
2025-03-05 12:43:42 +0530

    [Darshan] Add . extend Employee Payroll Data to store gender and start date

--- EmployeePAyroll.js
+++ EmployeePAyroll.js
@@ -3,11 +3,16 @@ class EmployeePayroll{
     id;
     name;
     salary;
+    gender;
+    startDate;
+
     //constructor
-    constructor(id, name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary; 
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2]; 
+        this.gender = params[3];
+        this.startDate = params[4]
     }
     //getter and setter
     get name(){
@@ -17,11 +22,16 @@ class EmployeePayroll{
         this.name = name;
     }
     toString(){
-        return "id = "+this.id+", name = "+this.name+", salary= "+this.salary;
+        return "id = "+this.id+", name = "+this.name+", salary= "+this.salary," gender= ",this.gender,", startDate= ",this.startDate;
     }
 }
 //create employee object
-let employeePayroll = new EmployeePayroll(1,"Rajveer",80000);
-console.log(employeePayroll.toString());
-employeePayroll.name = "Veer";
-console.log(employeePayroll.toString());
+let rajveerEmployeePayroll = new EmployeePayroll(1,"Rajveer",80000);
+console.log(rajveerEmployeePayroll.toString());
+rajveerEmployeePayroll.name = "Veer";
+console.log(rajveerEmployeePayroll.toString());
+
+//new empl0yee
+let darshanEmployee = new EmployeePayroll(2,"Darshan", 75000, "M", "25-02-2025")
+console.log(darshanEmployee.toString);
+
