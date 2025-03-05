798691b53b369bb4a534c6b2609c0fb326e1b8aa
drshny03 <darshanyadav7049@gmail.com>
2025-03-05 11:52:31 +0530

    [Darshan] Add. Calculating Wages for a Month

--- CalculateDailyWage.js
+++ CalculateDailyWage.js
@@ -4,17 +4,26 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+//method to calculate working hour
+function getWorkingHour(employeeCheck){
+    switch(employeeCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+//number of working day
+const NUMBER_OF_DAYS = 20;
 let employeeHour = 0;
-employeeCheck = Math.floor(Math.random()*10%3);
-switch(employeeCheck){
-    case IS_PART_TIME:
-        employeeHour = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        employeeHour = FULL_TIME_HOURS;
-        break
-    default:
-        employeeHour = 0;
+
+//calculate for 20 days
+for(let day = 0; day<NUMBER_OF_DAYS; day++){
+    let employeeCheck = Math.floor(Math.random()*10%3);
+    //call function to calculate working hour
+    employeeHour += getWorkingHour(employeeCheck)
 }
 
 let employeeWage = employeeHour * WAGE_PER_HOUR;
